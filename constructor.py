05ebf298ed984249fe83b6fb8dc3182f7327dd7f
abhinavaby <abhinavaby07@gmail.com>
2026-01-25 11:46:15 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,fullname,age):
+        self.fullname=fullname
+        self.age=age
+        print(f"hello {fullname} you are {age} years old")
+        print(self)
+
+
+fullname=input("enter fullname")
+age=int(input("enter age"))
+s1=student(fullname,age)
+print(s1)
